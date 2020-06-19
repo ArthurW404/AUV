9ba7788fd776d90fd404fd1d1c91a2e500d5bc48
Arthur Jia Jun Wang <z5257096@unsw.edu.au>
2020-06-19 23:55:25 +1000

    Tried out fast in videoCapture

--- python/videoCapture/sampleVideo.py
+++ python/videoCapture/sampleVideo.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
-
 def getBlue(img):
     hsvImg = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
@@ -16,20 +14,43 @@ def getBlue(img):
     res = cv.bitwise_and(img,img, mask=mask)
     return res
 
+def FAST(image):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    #create fast detector object
+    fast = cv.FastFeatureDetector_create()
 
-    blue = getBlue(frame)
-    # Our operations on the frame come here
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Obtain key points using FAST
+    keypoints = fast.detect(gray, None)
+    # num_kpts = len(keypoints)
+    # print(f"Number of keypoints Detected: {num_kpts}")
+    # print(keypoints[0])
+    # extract key points as list 
+    # pts = cv.KeyPoint_convert(keypoints)
+    # print(pts[0])
 
-    # Display the resulting framef
-    cv.imshow('frame',blue)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+    # draw rich keypoints on input image
+    image = cv.drawKeypoints(image, keypoints, outImage=None, color=[0, 0, 255],flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+    image = cv.resize(image, (1400, 900))
+    return image
+
+if __name__ == "__main__":
+    cap = cv.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        corners = FAST(frame)
+        # Our operations on the frame come here
+        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # Display the resulting framef
+        cv.imshow('frame',corners)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
